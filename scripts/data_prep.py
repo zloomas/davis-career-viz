2a9fa3c4d216cd84d8f02b798da1fe51edf986d5
Zach Loomas <zloomas@gmail.com>
2020-12-30 22:00:15 -0500

    update seed paper nodes
    use WOS ID instead of assigning new one

--- scripts/data_prep.py
+++ scripts/data_prep.py
@@ -1,10 +1,11 @@
 ## Mark Davis Career Vizualization Project
 
-#load pandas package
+#load packages
 import pandas as pd
+import glob
 
 #read in raw data
-wos_export = pd.read_csv("./data/savedrecs.txt", sep='\t', header=0, index_col=False, dtype={'PY': str}, usecols=['DT', 'AU', 'TI', 'SO', 'PY', 'CR', 'NR'])
+wos_export = pd.read_csv("./data/savedrecs.txt", sep='\t', header=0, index_col=False, dtype={'PY': str}, usecols=['DT', 'AU', 'TI', 'SO', 'PY', 'CR', 'NR', 'UT'])
 
 #make nodes df for author-wise network
 authors = []
@@ -20,7 +21,7 @@ author_nodes = pd.DataFrame(columns=['au_id', 'au_name'])
 
 for au in range(len(authors)):
     au_id = 'au' + str(au+1)
-    author_nodes = author_nodes.append({'au_id':au_id, 'au_name':authors[au]}, ignore_index=True)
+    author_nodes = author_nodes.append({'au_id': au_id, 'au_name': authors[au]}, ignore_index=True)
 
 #author_nodes.to_csv("./data/coauthors/author_network_nodes.tsv", sep='\t', index=False)
 
@@ -49,19 +50,28 @@ for entry in wos_export.index:
             ix += 1
 
 
-author_edges = author_edges[['from','to','title','journ','pub_year','doc_type']]
+author_edges = author_edges[['from', 'to', 'title', 'journ', 'pub_year', 'doc_type']]
 #author_edges.to_csv("./data/coauthors/author_network_edges.tsv", sep='\t', index=False)
 
 #ok - now instead, time to try to make an actual (tiny) citation network
-#start with list of his own papers to make some ids
-papers = [ti.upper() for ti in wos_export['TI']]
+#start with list of his own papers to get some ids - use inherent WOS IDs
 
 paper_nodes = pd.DataFrame(columns=['paper_id', 'title'])
 
-for ti in range(len(papers)):
-    paper_id = 'pa' + str(ti+1)
-    paper_nodes = paper_nodes.append({'paper_id':paper_id, 'title':papers[ti]}, ignore_index=True)
+for entry in wos_export.index:
+    paper_id = wos_export.loc[entry, 'UT'].split(':')[-1]
+    title = wos_export.loc[entry, 'TI'].upper()
+    new_entry = {'paper_id': paper_id,
+                 'title': title}
+    paper_nodes = paper_nodes.append(new_entry, ignore_index=True)
 
 #paper_nodes.to_csv("./data/papers/seed_paper_network_nodes.tsv", sep='\t', index=False)
+
+
+
+
+
+
+
 
 
