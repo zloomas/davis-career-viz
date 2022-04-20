6d8bea75b1583a7aff391dd59996dfacb91114de
Zach Loomas <47288744+zloomas@users.noreply.github.com>
2022-04-20 01:33:58 -0400

    add final product scripts

--- scripts/mark_focus_citation_viz.R
+++ scripts/mark_focus_citation_viz.R
@@ -1,28 +1,41 @@
 ## run setup to load networks for plot
 source(here::here("scripts", "viz_setup.R"))
 
+mark <- c(
+  "#fafa6e",
+  "#f7d253",
+  "#eeab42",
+  "#df853a",
+  "#ca6037"
+)
+
+blue <- c(
+  "#6dbae1",
+  "#439db4",
+  "#238088",
+  "#0f625d",
+  "#084537"
+)
+
+pub_colors <- append(blue, mark)
 
 ggraph(reduced_paper_network, layout = "kk") +
-  geom_edge_arc(
-    color = "grey90", 
-    aes(alpha=big_boy),
-    show.legend = FALSE
-  ) +
-  scale_edge_alpha_manual(values = c(.1, 0)) +
   geom_node_point(
     aes(
-      size = big_boy,
-      color = pub_decade
+      size = size_ref,
+      color = pub_decade_adjusted
     ),
     show.legend = FALSE
   ) +
-  scale_size_manual(values = c(.3, 3)) +
+  scale_size_identity() +
+  scale_color_manual(values = pub_colors) +
   theme_void() +
-  theme(plot.background = element_rect(fill = "black", color = "black"),
-        panel.background = element_rect(fill = "black", color = "black")) +
+  theme(
+    plot.background = element_rect(fill = "black", color = "black"),
+    panel.background = element_rect(fill = "black", color = "black")
+  ) +
   coord_flip() +
   scale_x_reverse()
 
-# try to turn edges off for things connected to big boy
-# make big boy bigger
-# continue to play with color
+
+
--- scripts/viz_setup.R
+++ scripts/viz_setup.R
@@ -33,18 +33,47 @@ mark_papers <- read_tsv(here("data","papers","seed_paper_network_nodes.tsv"))
 
 paper_nodes <- read_tsv(here("data","papers","paper_network_nodes.tsv")) %>%
   mutate(pub_decade = floor(pub_year/10),
-         big_boy = paper_id == "A1983PY32000010")
+         big_boy = paper_id == "A1983PY32000010",
+         is_mark = paper_id %in% mark_papers$paper_id) %>%
+  filter(!is.na(pub_decade))
 
 paper_edges <- read_tsv(here("data","papers","paper_network_edges.tsv")) %>%
-  mutate(citation_type = case_when(
-    to %in% mark_papers$paper_id & 
-      !(from %in% mark_papers$paper_id) ~ "cited_by_mark",
-    from %in% mark_papers$paper_id & 
-      !(to %in% mark_papers$paper_id) ~ "cites_mark",
-    to %in% mark_papers$paper_id & 
-      from %in% mark_papers$paper_id ~ "self-cite"),
+  mutate(
+    citation_type = case_when(
+      to %in% mark_papers$paper_id & 
+        !(from %in% mark_papers$paper_id) ~ "cited_by_mark",
+      from %in% mark_papers$paper_id & 
+        !(to %in% mark_papers$paper_id) ~ "cites_mark",
+      to %in% mark_papers$paper_id & 
+        from %in% mark_papers$paper_id ~ "self-cite"
+    ),
     big_boy = from == "A1983PY32000010" | to == "A1983PY32000010"
-  )
+  ) %>%
+  filter(to %in% paper_nodes$paper_id & from %in% paper_nodes$paper_id)
+
+paper_edges_summary <- paper_edges %>%
+  group_by(from) %>%
+  summarise(n = n()) %>%
+  mutate(log_n_plus = log10(n+1))
+
+paper_nodes <- merge(
+  paper_nodes, 
+  paper_edges_summary,
+  by.x = "paper_id",
+  by.y = "from",
+  all.x = TRUE
+) %>% mutate(
+  size_ref = case_when(is.na(log_n_plus) ~ log10(2),
+                       TRUE ~ log_n_plus)
+) %>% mutate(
+  size_ref = ceiling(size_ref)
+) %>% mutate(
+  size_ref = case_when(size_ref == 1 ~ 3,
+                       size_ref == 2 ~ 6,
+                       size_ref == 3 ~ 12,
+                       size_ref == 4 ~ 15,
+                       TRUE ~ 0)
+)
 
 paper_network <- graph_from_data_frame(
   d=paper_edges,
@@ -55,12 +84,21 @@ paper_network <- graph_from_data_frame(
 
 ## citation network with just "big boy"
 reduced_paper_edges <- filter(paper_edges,
-                              citation_type != "cited_by_mark")
+                              citation_type == "cites_mark")
 
 reduced_paper_list <- unique(c(reduced_paper_edges$from, reduced_paper_edges$to))
 
-reduced_paper_nodes <- filter(paper_nodes,
-                              paper_id %in% reduced_paper_list)
+reduced_paper_nodes <- paper_nodes %>%
+  filter(paper_id %in% reduced_paper_list) %>%
+  mutate(
+    pub_decade_adjusted = as.character(
+      ifelse(
+        is_mark,
+        pub_decade*2,
+        pub_decade 
+      ) 
+    )
+  )
 
 reduced_paper_network <- graph_from_data_frame(
   d=reduced_paper_edges, 
